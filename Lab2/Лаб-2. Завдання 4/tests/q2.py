f92b2d5b363628ff4d57fcd5350b6d3cac01a958
v3nux <105073614+v3nux@users.noreply.github.com>
2022-05-06 21:23:15 +0300

    Delete Lab2 directory

--- Lab2/Лаб-2. Завдання 4/tests/q2.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q2',
-    'points': 1,
-    'suites': [{'cases': [{'code': '>>> isinstance(my_info["month_of_birth"], str)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- Lab2/Лаб-2. Завдання 4/tests/q6.py
+++
@@ -1,1 +0,0 @@
-test = {'name': 'q6', 'points': 1, 'suites': [{'cases': [{'code': '>>> kol_uniq_symb == 26\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
